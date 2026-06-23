71a6f2400237faccf104d92e127b185b9406ca65
Juan Bautista Aballay <67266651+bautixter@users.noreply.github.com>
2026-06-23 19:54:44 -0300

    💄 Marginalia: handwritten editorial quote overlay + painting info block

--- src/rooms/01_Garabatos.jsx
+++ src/rooms/01_Garabatos.jsx
@@ -6,7 +6,7 @@ import { DEFAULT_ROOM_SPAWN as ROOM_SPAWN } from '../utils/roomSpawn'
 import { asset } from '../utils/asset'
 import Painting from '../components/Painting'
 
-const FOG_COLOR = '#bababa'
+const FOG_COLOR = '#b0dbff'
 
 export default function Garabatos() {
   const { scene } = useThree()
@@ -28,14 +28,22 @@ export default function Garabatos() {
       <directionalLight position={[0, 20, 0]} intensity={0.6} castShadow />
       <mesh rotation={[-Math.PI / 2, 0, 0]} receiveShadow>
         <planeGeometry args={[4000, 4000]} />
-        <meshStandardMaterial color="#ffffff" />
+        <meshStandardMaterial color="#41ad4a" />
       </mesh>
       <Painting
         src={asset('/images/d01_01.jpg')}
         width={1.5}
         canvasColor={'#ffffff'}
-        position={[0, 1.6, 0]}
+        position={[0, 1.6, -2.2]}
         rotation={[0, Math.PI, 0]}
+        spotlight={{ intensity: 5, color: '#ffffff', angle: 0.7, penumbra: 0.6, front: 1, up: 2, targetY: 0, decay: 1, castShadow: false }}
+        info={{
+          header: 'Corea',
+          subheader: 'J.B. Aballay - 2023',
+          tags: ['Digital', 'Doodles'],
+          description: 'Collection of drawings from my stay in Busan, Republic of Korea',
+          color: '#2b211b',
+        }}
       />
     </RoomSpawnAlign>
   )
